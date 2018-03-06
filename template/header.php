7aedca720559b184dbc720744534d36185d032e8
2D192K <ethan.smith2281@gmail.com>
2018-03-06 17:48:23 +1100

    OH SHIT SON WE GOT A HEADING TEMPLATE

---
+++ template/header.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+  		<title>Jaffy &mdash; <?php echo $title ?></title>
+  		<link rel="stylesheet" type="text/css" href="/css/style.css"/>	
+  </head>
+  <body>
+		<header>
+			<h1><a href="/" id="logo">Jfy<div class="blink">_</div></a><?php echo $heading ?></h1>
+		</header>
+	  <section class="linklist">
+			<a href="https://github.com/2D192K" ><label>GitHub</label></a>
+			<a href="/contact"><label>Contact</label></a>
+			<a href="/projects"><label>Projects</label></a>
+			<a href="/mods"><label>Mods</label></a>
+		</section>
+	</body>
+</html>
