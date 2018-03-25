53d520e4ae44a7bffec24081be92c2bb4a6ef705
2D192K <ethan.smith2281@gmail.com>
2018-03-26 01:42:46 +1100

    Update datscraper.js

--- datscraper.js
+++ datscraper.js
@@ -1,21 +1,18 @@
-//inputValue is the shit we want baby
-//url: 'https://spreadsheets.google.com/feeds/cells/1ettowHimvIrLHkSRYs5VNQJQVPBv6qHai_PIBIKpjbA/default/public/full/R' + i + 'C1?alt=json'
-//csv: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS1RfrWSUM5uQh4LSFrEyG9L6ue755KPCqXlISQqROG2h66BHyELsprSJigthSWP_fcUtXcjw81R_a4/pub?output=csv'
-//jQuery.getJSON( url [, data ] [, success ] )
-var ListArr = [];
-for (i=1;i<562;i++){
-
+function generateList() {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			var myObj = JSON.parse(this.responseText);
-			var node=document.createElement("LI");
-			var textnode=document.createTextNode(myObj.entry.gs$cell.inputValue);
-			node.appendChild(textnode);
-			document.getElementById("bigList").appendChild(node);
+			var rawList = JSON.parse(this.responseText);
+			for (i=0;i<562;i++) {
+				console.log(rawList['feed']['entry'][i]['content']['$t']);
+				var title=document.createTextNode(rawList['feed']['entry'][i]['content']['$t']);
+				var node=document.createElement("LI");
+				node.appendChild(title);
+				document.getElementById("bigList").appendChild(node);
+				}
 		}
 	};
-	xmlhttp.open("GET", 'https://spreadsheets.google.com/feeds/cells/1ettowHimvIrLHkSRYs5VNQJQVPBv6qHai_PIBIKpjbA/default/public/full/R'+ i +'C1?alt=json', true);
+	xmlhttp.open("GET", 'https://spreadsheets.google.com/feeds/cells/1ettowHimvIrLHkSRYs5VNQJQVPBv6qHai_PIBIKpjbA/1/public/values?alt=json', true);
 	xmlhttp.send();
 }
-//document.getElementById("bigList").appendChild(document.createElement("LI").appendChild(document.createTextNode(myObj.entry.content.$t)));
+generateList();
